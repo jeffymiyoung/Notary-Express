0624ed488d2ab99a5e52d20a901698d72ad7c917
Jeffrey Young <jeffreymiyoung@aol.com>
2022-06-05 09:44:14 -0400

    added post/delete methods

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -16,6 +16,35 @@ router.get('/notes', function (req, res) {
 });
 
 // POST new note
+router.post('/notes', function (req, res) {
+    let newNote = req.body;
+    let lastId = 0;
+    if (noteContents.length !== 0) {
+        lastId = noteContents[noteContents.length -1]["id"];
+    }
+    let newId = lastId + 1;
+    newNote["id"] = newId;
+    noteContents.push(newNote);
+    writeFileAsync("db/db.json", JSON.stringify(noteContents)).then(function () {
+        console.log("Note has been updated!");
+    });
+    res.json(newNote);
+});
+
+// DELETE note
+router.delete('/notes/:id', function (req, res) {
+    let chosenId = parseInt(req.params.id);
+    for (let i = 0; i < noteContents.length; i++) {
+        if (chosenId === noteContents[i].id) {
+            noteContents.splice(i, 1);
+            let noteJSON = JSON.stringify(noteContents, null, 2);
+            writeFileAsync('db/db.json', noteJSON).then(function () {
+                console.log("Note has been deleted!");
+            });
+        }
+    }
+    res.json(noteContents);
+});
 
 // Export for External
 module.exports = router;
